d7d1113336351367353f659d86ca620cc27e5041
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-07-17 14:16:43 -0700

    reverse string

--- reverse-string/solution.js
+++ reverse-string/solution.js
@@ -5,3 +5,21 @@ const solution = module.exports = {}
 solution.reverseString = (str) => {
   return str.split('').reverse().join('')
 }
+
+solution.reverseString2 = str => {
+  let reverse = ''
+
+  for (let i = 0; i < str.length + 1; i++) {
+    reverse += str.charAt(str.length - i)
+  }
+  return reverse
+}
+
+solution.reverseString3 = str => {
+  let reverse = ''
+
+  for (let temp of str) {
+    reverse = temp + reverse
+  }
+  return reverse
+}
