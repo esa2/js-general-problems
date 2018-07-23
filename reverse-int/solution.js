44e4f6317cd7284f27ccea745eed2c1307f75c91
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-07-23 15:19:13 -0700

    reverse integer

--- reverse-int/solution.js
+++ reverse-int/solution.js
@@ -2,7 +2,7 @@
 
 const solution = module.exports = {}
 
-solution.reverseInt = (int) => {
+solution.reverseInt = int => {
   let revInt = int.toString().split('').reverse().join('')
 
   return parseInt(revInt) * Math.sign(int)
