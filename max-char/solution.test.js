f77c32b11d13ef1743e6af72a48efba305036111
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-08-01 17:59:32 -0700

    max char

--- max-char/solution.test.js
+++ max-char/solution.test.js
@@ -8,4 +8,14 @@ describe('Solution Module', function() {
       expect(solution.maxChar('abcvabzzzzzv')).toEqual(5)
     })
   })
+    describe('#maxChar', function() {
+      it('should return the character that occurs most frequently', function() {
+        expect(solution.maxChar2('11223330')).toEqual(3)
+      })
+    })
+    describe('#maxChar', function() {
+      it('should return the character that occurs most frequently', function() {
+        expect(solution.maxChar3('a  b  ')).toEqual(4)
+      })
+    })
 })
