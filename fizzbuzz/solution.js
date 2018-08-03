5c6689205c0403a3d0ec40ed1c5d4a207a5fcb4d
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-08-02 17:30:15 -0700

    fizzbuzz

--- fizzbuzz/solution.js
+++ fizzbuzz/solution.js
@@ -2,6 +2,19 @@
 
 const solution = module.exports = {}
 
-solution.fizzbuzz = str => {
-  
+solution.fizzbuzz = n => {
+  let result = ''
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result += 'fizzbuzz '
+    } else if (i % 3 === 0) {
+      result += 'fizz '
+    } else if (i % 5 === 0) {
+      result += 'buzz '
+    } else {
+      result += i + ' '
+    }
+  }
+  return result
 }
