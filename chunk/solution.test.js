352aece4799c92e3ec8785dc74a91b0766255b29
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-08-06 19:10:00 -0700

    chunk

--- chunk/solution.test.js
+++ chunk/solution.test.js
@@ -6,4 +6,7 @@ describe('#chunk', function() {
   it('should return', function() {
     expect(solution.chunk([1, 2, 3, 4], 2)).toEqual([[1, 2], [3, 4]])
   })
+  it('should return', function() {
+    expect(solution.chunk2([1, 2, 3, 4, 2])).toEqual([[1, 2], [3, 4]])
+  })
 })
