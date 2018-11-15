0c3e6fa5ad4d647008af463d4a19d8aa7a7628e4
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-11-15 13:02:56 -0800

    add comment

--- anagrams/solution.test.js
+++ anagrams/solution.test.js
@@ -4,12 +4,12 @@ const solution = require('./solution')
 
 describe('Solution Module', function() {
   describe('#anagram', function() {
-    it('should return', function() {
+    it('should return true', function() {
       expect(solution.anagram('abc', 'abc')).toEqual(true)
     })
   })
   describe('#anagram', function() {
-    it('should return', function() {
+    it('should return false', function() {
       expect(solution.anagram('abc', 'abcd')).toEqual(false)
     })
   })
